fc6f6b27f1bb67c4da89241bc3ccb97234e6994a
radness <yhs5128@gmail.com>
2024-08-05 12:25:57 +0900

    dashboard & card

--- src/components/Dashboard/Card/index.tsx
+++ src/components/Dashboard/Card/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './card.css';
+import CardFilter from "../CardFilter";
 
 const Card = ({card}: {card: any}) => {
+    const [filter, setFilter] = useState<string>('Today');
+    const handleFilterChange = (filter: React.SetStateAction<string>) => {
+        setFilter(filter);
+    };
+
     return (
-        <div>{card.name}</div>
+        // <div>{card.name}</div>
+        <div className='col-xxl-4 col-md-6'>
+         <div className='card info-card sales-card'>
+             <CardFilter filterChange={handleFilterChange} />
+             <div className='card-body'>
+                 <h5 className='card-title'>
+                     {card.name} <span>| {filter}</span>
+                 </h5>
+                 <div className='d-flex align-items-center'>
+                     <div className='card-icon rounded-circle d-flex align-items-center justify-content-center'>
+                         <i className={card.icon}></i>
+                     </div>
+                     <div className='ps-3'>
+                         <h6>
+                             {card.name === 'Revenue'
+                                 ? '$' + card.amount.toLocaleString('en-US')
+                                 : card.amount.toLocaleString('en-US')
+                             }
+                         </h6>
+                         <span
+                            className={`${
+                                card.percentage > 0 ? 'text-success' : 'text-danger'
+                            } small pt-1 fw-bold`}
+                         >
+                            {card.percentage > 0 ? card.percentage * 100 : -card.percentage * 100}%
+                         </span>
+                         <span className='text-muted small pt-2 ps-1'>
+                             {card.percentage > 0 ? 'increase' : 'decrease'}
+                         </span>
+                     </div>
+                 </div>
+             </div>
+         </div>
+        </div>
     );
 };
 
