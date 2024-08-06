bf02895aaa4f8c039083daeaadbc1518c0ad2e45
radness <yhs5128@gmail.com>
2024-08-06 21:38:45 +0900

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import './App.css';
 import {Header} from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
             <Header />
             <Sidebar />
             <Main />
+            <Footer/>
         </>
     );
 }
