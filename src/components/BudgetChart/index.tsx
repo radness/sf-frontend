294cc0542571f9c4d0620dad553538659407fe26
radness <yhs5128@gmail.com>
2024-08-07 15:27:50 +0900

    add eChart

--- src/components/BudgetChart/index.tsx
+++ src/components/BudgetChart/index.tsx
@@ -3,65 +3,46 @@ import * as echarts from 'echarts';
 
 const BudgetChart = () => {
     useEffect(() => {
-        const budgetReportChart = echarts.init(document.querySelector('#budgetChart') as HTMLElement);
-        budgetReportChart.setOption({
+        echarts.init(document.querySelector('#budgetChart') as HTMLElement).setOption({
             legend: {
-                data: ['Allocated Budget', 'Actual Spending'],
+                data: ['Allocated Budget', 'Actual Spending']
             },
             radar: {
-                shape: 'circle',
+                // shape: 'circle',
                 indicator: [
-                    {
-                        name: 'Sales',
-                        max: 6500,
-                    },
-                    {
-                        name: 'Administration',
-                        max: 16000,
-                    },
-                    {
-                        name: 'Information Technology',
-                        max: 30000,
-                    },
-                    {
-                        name: 'Customer Support',
-                        max: 38000,
-                    },
-                    {
-                        name: 'Development',
-                        max: 52000,
-                    },
-                    {
-                        name: 'Marketing',
-                        max: 25000,
-                    },
-                ],
+                    { name: 'Sales', max: 6500 },
+                    { name: 'Administration', max: 16000 },
+                    { name: 'Information Technology', max: 30000 },
+                    { name: 'Customer Support', max: 38000 },
+                    { name: 'Development', max: 52000 },
+                    { name: 'Marketing', max: 25000 }
+                ]
             },
             series: [
                 {
-                    name: 'Budget vs Spending',
+                    name: 'Budget vs spending',
                     type: 'radar',
                     data: [
                         {
                             value: [4200, 3000, 20000, 35000, 50000, 18000],
-                            name: 'Allocated Budget',
+                            name: 'Allocated Budget'
                         },
                         {
                             value: [5000, 14000, 28000, 26000, 42000, 21000],
-                            name: 'Actual Spending',
+                            name: 'Actual Spending'
                         },
-                    ],
-                },
-            ],
+                    ]
+                }
+            ]
         });
     }, []);
 
     return (
+        // eChart 사용 시 크기를 미리 지정하지 않으면 렌더링이 안되는 문제점이 있다.
         <div id="budgetChart"
-             style={{ maxHeight: '400px' }}
+             style={{ width: '400px', height: '400px', maxHeight: '400px' }}
              className="echart"
         >
-            chart
         </div>
     );
 };
--- src/components/Sidebar/NavItem/index.tsx
+++ src/components/Sidebar/NavItem/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavItem = ({item} : {item:any}) => {
     return (
-        <li className='nav-item' key='item._id'>
+        <li className='nav-item' key={item._id}>
             <a className='nav-link collapsed' href='#'>
                 <i className={item.icon}></i>
                 <span>{item.name}</span>
